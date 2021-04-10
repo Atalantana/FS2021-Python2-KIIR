af8d7f3e2708009a01e929cbe2e5998a81aaf708
Atalantana <36230941+Atalantana@users.noreply.github.com>
2021-04-10 19:20:57 +0200

    Formulardaten werden nun auf neuer Result-Page angezeigt.Anpassung an HTML mit Jinja, Anpassung main.py Datei  für den Formulardaten-Abfang.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from flask import request
 from flask import Flask
 from flask import render_template
 
@@ -13,9 +14,25 @@ def home():
 def meinePflanzen():
     return render_template("meinePflanzen.html")
 
+
+@app.route("/meinePflanzen", methods=["GET", "POST"])
+def result():
+    if request.method == "POST":
+        pftyp = request.form["pflanzenauswahl"]
+        pfname = request.form["Pflanzenname"]
+        kfdatum = request.form["Kaufdatum"]
+        wsdatum = request.form["Wassergabe"]
+        result= pftyp+" "+pfname+" "+kfdatum+" "+wsdatum
+    return render_template("/result.html", result=result )
+
+
+
+
 @app.route("/Materialverwaltung")
 def materialverwlatung():
     return render_template("Materialverwaltung.html")
+
+
 
 
 if __name__ == "__main__":
