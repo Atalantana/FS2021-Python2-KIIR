6641425c2fcb6e6300fe2fbd3ac3e05fd8ed5142
Atalantana <36230941+Atalantana@users.noreply.github.com>
2021-05-06 14:08:53 +0200

    Formulardaten werden nun nach dem Befehl "Submit" in ein separates JSON File geschrieben und gespeichert.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import request
 from flask import Flask
 from flask import render_template
+from flask import json
 
 app = Flask("Peabuddy")
 
@@ -15,17 +16,35 @@ def meinePflanzen():
     return render_template("meinePflanzen.html")
 
 
-@app.route("/meinePflanzen", methods=["GET", "POST"])
+@app.route("/result", methods=["GET", "POST"])
 def result():
+    data_plants = {}
     if request.method == "POST":
         pftyp = request.form["pflanzenauswahl"]
         pfname = request.form["Pflanzenname"]
         kfdatum = request.form["Kaufdatum"]
         wsdatum = request.form["Wassergabe"]
-        result= pftyp+" "+pfname+" "+kfdatum+" "+wsdatum
-    return render_template("/result.html", result=result )
+        data_plants["Pflanzen"] = []
+        data_plants["Pflanzen"].append({
+                "Pflanzentyp": pftyp,
+                "Pflanzenname": pfname,
+                "Kaufdatum": kfdatum,
+                "Letzte Wassergabe":wsdatum
+        })
+    with open("data_plants.txt", "w") as outfile:
+        json.dump(data_plants, outfile)
+    return render_template("result.html", result=result)
 
-
+#@app.route("/result", methods=["GET", "POST"])
+#def result():
+    #data_plants = {}
+    #if request.method == "POST":
+        #pftyp = request.form["pflanzenauswahl"]
+        #pfname = request.form["Pflanzenname"]
+        #kfdatum = request.form["Kaufdatum"]
+        #wsdatum = request.form["Wassergabe"]
+        #result= #pftyp+" "+pfname+" "+kfdatum+" "+wsdatum
+    #return render_template("result.html", result=result)
 
 
 @app.route("/Materialverwaltung")
