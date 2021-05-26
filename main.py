16b974deeb94792fd60bad35096b4d1308f5f70e
Atalantana <36230941+Atalantana@users.noreply.github.com>
2021-05-26 20:55:39 +0200

    Den Einträgen in der Liste bzw. den Dictionaries wird nun  mit jedem Eintrag eine ID hinzugefügt. Diese basiert auf der Anzahl Einträge (Dictionaries) in der Liste.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import request
 from flask import Flask
 from flask import render_template
 from flask import json
+import tkinter as tk
 from jinja2 import Environment
 import pandas as pd
 import csv
@@ -19,11 +20,13 @@ def home():
 def formular():
     pflanzen = []
     data_plants = {}
+
     if request.method == "POST":
-        pftyp = request.form["pflanzenauswahl"]
+        pftyp = request.form["Pflanzentyp"]
         pfname = request.form["Pflanzenname"]
         kfdatum = request.form["Kaufdatum"]
         wsdatum = request.form["Wassergabe"]
+
         data_plants["Pflanzentyp"] = pftyp
         data_plants["Pflanzenname"] = pfname
         data_plants["Kaufdatum"] = kfdatum
@@ -31,7 +34,8 @@ def formular():
         with open("data_plants.json", "r+") as outfile:
             pflanzen = json.load(outfile)
             pflanzen.append(data_plants)
-
+            count = len(pflanzen)
+            data_plants["ID"] = count
         with open("data_plants.json", "w") as outfile:
             json.dump(pflanzen, outfile, indent=4)
         return render_template("meinePflanzen.html", anzeige=data_plants)
@@ -48,10 +52,15 @@ def anzeigeAlle():
     return render_template("AllePflanzen.html", anzeige=False)
 
 
-def bearbeiten():
-
-    return render_template("AllePflanzen.html", a=a)
-
+def loeschen():
+    with open("data_plants.json", "r+") as outfile:
+        pflanzen = json.load(outfile)
+    for i in range (len(pflanzen)):
+        if pflanzen[i]==len(pflanzen):
+            del pflanzen[i]
+            break
+    return render_template("AllePflanzen.html", anzeige=pflanzen)
+    return render_template("AllePflanzen.html", anzeige=False)
 
 @app.route("/Materialverwaltung", methods=["GET", "POST"])
 def materialverwaltung():
