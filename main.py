c6aae81b4a4163c27bc2def28dd77a36d514a48a
Atalantana <36230941+Atalantana@users.noreply.github.com>
2021-05-28 15:19:07 +0200

    Die ID wurde nun mit einer Timestamp ersetzt, da die vorherige Nummern vergabe zu Problemen bei der Löschung einzelner Einträge geführt hätte.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import request
 from flask import Flask
 from flask import render_template
 from flask import json
+from datetime import datetime
 import tkinter as tk
 from jinja2 import Environment
 import pandas as pd
@@ -34,8 +35,9 @@ def formular():
         with open("data_plants.json", "r+") as outfile:
             pflanzen = json.load(outfile)
             pflanzen.append(data_plants)
-            count = len(pflanzen)
-            data_plants["ID"] = count
+            now = datetime.now()
+            timestamp = datetime.timestamp(now)
+            data_plants["Timestamp"]=timestamp
         with open("data_plants.json", "w") as outfile:
             json.dump(pflanzen, outfile, indent=4)
         return render_template("meinePflanzen.html", anzeige=data_plants)
@@ -43,12 +45,12 @@ def formular():
         return render_template("meinePflanzen.html", anzeige=False)
 
 
-
 @app.route("/AllePflanzen", methods=["GET", "POST"])
 def anzeigeAlle():
     with open("data_plants.json", "r+") as outfile:
         pflanzen = json.load(outfile)
-    return render_template("AllePflanzen.html", anzeige=pflanzen)
+    count = len(pflanzen)
+    return render_template("AllePflanzen.html", anzeige=pflanzen, anzeigec=count)
     return render_template("AllePflanzen.html", anzeige=False)
 
 
