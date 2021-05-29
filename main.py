252d8759b0cfc1f2539fc6039fe4649ae5c25041
Atalantana <36230941+Atalantana@users.noreply.github.com>
2021-05-29 20:50:25 +0200

    Es ist nun möglich, die Listeneinträge anhand ihrer Timestamps zu löschen.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask import render_template
 from flask import json
 from datetime import datetime
+from flask import Flask, render_template, request, redirect, url_for
 import tkinter as tk
 from jinja2 import Environment
 import pandas as pd
@@ -37,7 +38,7 @@ def formular():
             pflanzen.append(data_plants)
             now = datetime.now()
             timestamp = datetime.timestamp(now)
-            data_plants["Timestamp"]=timestamp
+            data_plants["Timestamp"]= str(timestamp)
         with open("data_plants.json", "w") as outfile:
             json.dump(pflanzen, outfile, indent=4)
         return render_template("meinePflanzen.html", anzeige=data_plants)
@@ -49,24 +50,50 @@ def formular():
 def anzeigeAlle():
     with open("data_plants.json", "r+") as outfile:
         pflanzen = json.load(outfile)
-    count = len(pflanzen)
-    return render_template("AllePflanzen.html", anzeige=pflanzen, anzeigec=count)
-    return render_template("AllePflanzen.html", anzeige=False)
-
-
-def loeschen():
-    with open("data_plants.json", "r+") as outfile:
-        pflanzen = json.load(outfile)
-    for i in range (len(pflanzen)):
-        if pflanzen[i]==len(pflanzen):
-            del pflanzen[i]
-            break
     return render_template("AllePflanzen.html", anzeige=pflanzen)
     return render_template("AllePflanzen.html", anzeige=False)
+
+
+@app.route("/loesch", methods=["GET", "POST"])
+def loesch():
+    with open("data_plants.json", "r+") as outfile:
+        pflanzen = json.load(outfile)
+    if request.method == 'POST':
+        timest = request.form["delpfl"]
+        for eintr in pflanzen:
+            timest_json = eintr["Timestamp"]
+            print("JSON", timest_json)
+            print("Form", timest)
+            if timest == timest_json:
+                pflanzen.remove(eintr)
+            with open("data_plants.json", "w") as outfile:
+                json.dump(pflanzen, outfile, indent=4)
+        return redirect(url_for("anzeigeAlle"))
+    return redirect(url_for("anzeigeAlle"))
+
+
+@app.route("/bearbeit", methods=["GET", "POST"])
+def bearbeit():
+    with open("data_plants.json", "r+") as outfile:
+        pflanzen = json.load(outfile)
+    if request.method == 'POST':
+        timest = request.form["beabpfl"]
+        for eintr in pflanzen:
+            timest_json = eintr["Timestamp"]
+            print("JSON", timest_json)
+            print("Form", timest)
+            if timest == timest_json:
+                pflanzen.remove(eintr)
+            with open("data_plants.json", "w") as outfile:
+                json.dump(pflanzen, outfile, indent=4)
+        return redirect(url_for("anzeigeAlle"))
+    return redirect(url_for("anzeigeAlle"))
+
 
 @app.route("/Materialverwaltung", methods=["GET", "POST"])
 def materialverwaltung():
     return render_template("Materialverwaltung.html")
+
 
 
 
