b4dc820d106a2ccda0ff9650ba1a685d6067351c
Atalantana <36230941+Atalantana@users.noreply.github.com>
2021-06-06 20:10:57 +0200

    Auf der Seite "Statistiken" wird nun eine Pie-Chart mit den jeweils vorkommenden Pflanzentypen angezeigt.

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ from flask import render_template
 from flask import json
 from datetime import datetime
 from flask import Flask, render_template, request, redirect, url_for
+from collections import Counter
 import easygui
 import plotly.express as px
+from plotly.offline import plot
 import tkinter as tk
 from jinja2 import Environment
 import pandas as pd
 import csv
+from IPython.display import display
 
 
 app = Flask("Peabuddy")
@@ -60,7 +63,10 @@ def formular():
 def anzeigeAlle():
     with open("data_plants.json", "r+") as outfile:
         pflanzen = json.load(outfile)
-    return render_template("AllePflanzen.html", anzeige=pflanzen)
+        pflanzen_sorted = sorted(pflanzen, key=lambda k: k['Wassergabe'])
+
+
+    return render_template("AllePflanzen.html", anzeige=pflanzen_sorted)
     return render_template("AllePflanzen.html", anzeige=False)
 
 
@@ -82,12 +88,16 @@ def loesch():
     return redirect(url_for("anzeigeAlle"))
 
 
-
 @app.route("/Statistiken", methods=["GET", "POST"])
 def statistiken():
-    return render_template("Statistiken.html")
-
-
+    df = pd.read_json('data_plants.json')
+    c = Counter(df["Pflanzentyp"])
+    liste = c.most_common()
+    p_type = [x[0] for x in liste]
+    p_size = [x[1] for x in liste]
+    fig = px.pie(names=p_type, values=p_size, title="Anzahl Pflanzentypen")
+    viz_pie = plot(fig, output_type="div")
+    return render_template("Statistiken.html", anzeige=viz_pie)
 
 
 if __name__ == "__main__":
